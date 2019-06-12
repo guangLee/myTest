fe04b9b45d89e1af0f31a4e1d36b8931b861da85
wb-lhg427173 <wb-lhg427173@autonavi.com>
2019-06-12 21:47:24 +0800

    提交两个测试类

---
+++ src/main/java/com/lihg/test/TestSerializable.java
@@ -0,0 +1,55 @@
+package com.lihg.test;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
+
+/**
+ * 通过反序列化的方式，从输入流中强转获得一个新的对象
+ */
+public class TestSerializable implements Serializable {
+//public class TestSerializable {
+
+    private String name;
+
+    public TestSerializable() {
+    }
+
+    public TestSerializable(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public static void main(String[] args) throws Exception {
+
+        String filePath = System.getProperty("user.dir") + "/sample.txt";
+        TestSerializable t1 = new TestSerializable("张三");
+        System.out.println("t1的哈希值:\t"+t1.hashCode());
+        try {
+            FileOutputStream fileOutputStream = new FileOutputStream(filePath);
+            ObjectOutputStream outputStream = new ObjectOutputStream(fileOutputStream);
+            outputStream.writeObject(t1);
+            outputStream.flush();
+            outputStream.close();
+
+            FileInputStream fileInputStream = new FileInputStream(filePath);
+            ObjectInputStream inputStream = new ObjectInputStream(fileInputStream);
+            TestSerializable t2 = (TestSerializable)inputStream.readObject();
+            inputStream.close();
+
+            System.out.println(t2.getName());
+            System.out.println("t2的哈希值:\t"+t2.hashCode());
+        } catch (Exception ee) {
+            ee.printStackTrace();
+        }
+    }
+}
---
+++ src/main/java/com/lihg/test/ThreadDemo.java
@@ -0,0 +1,40 @@
+package com.lihg.test;
+
+import java.util.concurrent.TimeUnit;
+
+public class ThreadDemo implements Cloneable{
+
+    public synchronized void  method1(){
+        //public  void  method1(){
+        //public static synchronized void  method1(){
+
+        System.out.println(this.hashCode()+"\t"+Thread.currentThread().getName()+"\t"+System.currentTimeMillis());
+        try {
+            TimeUnit.SECONDS.sleep(2);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("m1");
+
+    }
+    public synchronized void  method2(){
+
+        System.out.println(this.hashCode()+"\t"+Thread.currentThread().getName()+"\t"+System.currentTimeMillis());
+        try {
+            TimeUnit.SECONDS.sleep(2);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("m2");
+    }
+    public static void main(String[] args) {
+
+        ThreadDemo t = new ThreadDemo();
+        ThreadDemo t2 = new ThreadDemo();
+
+        new Thread(()->t.method1(),"1").start();
+        new Thread(()->t.method2(),"2").start();
+        new Thread(()->t2.method2(),"3").start();
+    }
+}
+
