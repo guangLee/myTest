717fe11e432a808cd6fd1607268a3bd0a2c6b7dd
wb-lhg427173 <wb-lhg427173@autonavi.com>
2019-06-12 16:25:09 +0800

    再测试一下提交好使不

--- src/main/java/com/lihg/test/MyTest.java
+++ src/main/java/com/lihg/test/MyTest.java
@@ -2,8 +2,8 @@ package com.lihg.test;
 
 public class MyTest {
 
-
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("我再次提交下试试");
     }
 }
